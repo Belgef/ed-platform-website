ff292c406f177c9120702e8d0df23ff09b59eb74
Belgef <girnyimykola@gmail.com>
2022-02-02 22:45:31 +0200

    Fixed null exception in sidebar

---
+++ Data/Migrations/20220202204323_Reset removed unnecessary nullables.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EdPlatformWebsite.Data.Migrations
+{
+    public partial class Resetremovedunnecessarynullables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LastExerciseAttempts_Exercises_ExerciseId",
+                table: "LastExerciseAttempts");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ExerciseId",
+                table: "LastExerciseAttempts",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "LastExerciseAttempts",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Output",
+                table: "IOCases",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Input",
+                table: "IOCases",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LastExerciseAttempts_Exercises_ExerciseId",
+                table: "LastExerciseAttempts",
+                column: "ExerciseId",
+                principalTable: "Exercises",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LastExerciseAttempts_Exercises_ExerciseId",
+                table: "LastExerciseAttempts");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ExerciseId",
+                table: "LastExerciseAttempts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "LastExerciseAttempts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Output",
+                table: "IOCases",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Input",
+                table: "IOCases",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LastExerciseAttempts_Exercises_ExerciseId",
+                table: "LastExerciseAttempts",
+                column: "ExerciseId",
+                principalTable: "Exercises",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
