8c379e78753e4a15cb03b610e5ac158bfdc854c9
Belgef <girnyimykola@gmail.com>
2022-02-05 17:59:54 +0200

    Added lesson editor template

---
+++ wwwroot/js/editor.jsx
@@ -0,0 +1,8 @@
+﻿class Editor extends React.Component {
+    render() {
+        return (
+            <h1>Hello</h1>
+            )
+    }
+}
+ReactDOM.render(< Editor />, document.getElementById('editor'));
