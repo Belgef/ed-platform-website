4a0899f78afc16a87f875c64095e4793d47f1fc2
Belgef <girnyimykola@gmail.com>
2022-02-07 20:07:36 +0200

    Added Ace code editor

---
+++ wwwroot/js/code-editor.es5.js
@@ -0,0 +1,7 @@
+﻿"use strict";
+
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/crimson_editor");
+editor.session.setMode("ace/mode/python");
+document.getElementById('editor').style.fontSize = '12px';
+
